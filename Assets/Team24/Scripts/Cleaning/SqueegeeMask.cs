cbc0cefc55168b955f5bef9dc0562b050c31f0b2
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-05 15:48:08 -0500

    Added dirt textures for testing

--- Assets/Team24/Scripts/Cleaning/SqueegeeMask.cs
+++ Assets/Team24/Scripts/Cleaning/SqueegeeMask.cs
@@ -2,47 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SqueegeeMask : MonoBehaviour
+namespace team24
 {
-    // This class makes a trail of gameobjects as this object moves
-    // This is to prevent gaps in the squeegee due to moving too far
-    // in between frames.
-
-    public int lerpCopies = 20;
-    public GameObject squeegeePrefab;
-
-    List<Transform> copies = new List<Transform>();
-    Vector3 lastPosition;
-    Quaternion lastRotation;
-
-    private void Start()
+    public class SqueegeeMask : MonoBehaviour
     {
-        lastPosition = transform.position;
-        lastRotation = transform.rotation;
-        for (int i = 0; i < lerpCopies; i++)
-            copies.Add(Instantiate(squeegeePrefab, transform).transform);
-    }
+        // This class makes a trail of gameobjects as this object moves
+        // This is to prevent gaps in the squeegee due to moving too far
+        // in between frames.
 
-    private void LateUpdate()
-    {
-        PositionCopies();
-        lastPosition = transform.position;
-        lastRotation = transform.rotation;
-    }
+        public int lerpCopies = 20;
+        public GameObject squeegeePrefab;
 
-    void PositionCopies()
-    {
-        Vector3 start = lastPosition;
-        Vector3 end = transform.position;
+        List<Transform> copies = new List<Transform>();
+        Vector3 lastPosition;
+        Quaternion lastRotation;
 
-        Quaternion startRot = lastRotation;
-        Quaternion endRot = transform.rotation;
-
-        for (int i = 0; i < copies.Count; i++)
+        private void Start()
         {
-            float fac = (float)i / copies.Count;
-            copies[i].position = Vector3.Lerp(start, end, fac);
-            copies[i].rotation = Quaternion.Slerp(startRot, endRot, fac);
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+            for (int i = 0; i < lerpCopies; i++)
+                copies.Add(Instantiate(squeegeePrefab, transform).transform);
+        }
+
+        private void LateUpdate()
+        {
+            PositionCopies();
+            lastPosition = transform.position;
+            lastRotation = transform.rotation;
+        }
+
+        void PositionCopies()
+        {
+            Vector3 start = lastPosition;
+            Vector3 end = transform.position;
+
+            Quaternion startRot = lastRotation;
+            Quaternion endRot = transform.rotation;
+
+            for (int i = 0; i < copies.Count; i++)
+            {
+                float fac = (float)i / copies.Count;
+                copies[i].position = Vector3.Lerp(start, end, fac);
+                copies[i].rotation = Quaternion.Slerp(startRot, endRot, fac);
+            }
         }
     }
 }
