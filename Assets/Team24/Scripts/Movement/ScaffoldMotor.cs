f47ba2a2d484ea8b910d116c5598939ac736a17a
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-06 00:13:40 -0500

    Clamped scaffolding to remain inside screen

--- Assets/Team24/Scripts/Movement/ScaffoldMotor.cs
+++ Assets/Team24/Scripts/Movement/ScaffoldMotor.cs
@@ -7,9 +7,12 @@ namespace team24
 {
     public class ScaffoldMotor : MicrogameInputEvents
     {
-        Vector2 direction;
         [SerializeField] float buttonSpeed;
         [SerializeField] float joystickSpeed;
+        [SerializeField] Vector2 xBounds;
+        [SerializeField] Vector2 yBounds;
+
+        Vector2 direction;
 
         public static bool UsingButtons { get; private set; }
 
@@ -68,6 +71,21 @@ namespace team24
                 // The squeegee can go
                 UsingButtons = false;
             }
+
+            // Make sure we don't go out of bounds
+            float clampedX = Mathf.Clamp(transform.position.x, xBounds.x, xBounds.y);
+            float clampedY = Mathf.Clamp(transform.position.y, yBounds.x, yBounds.y);
+            transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+        }
+
+        private void OnDrawGizmos()
+        {
+            // Draw the limits of where we can go
+            Gizmos.color = Color.yellow;
+            Vector3 min = new Vector3(xBounds.x, yBounds.x, -1f);
+            Vector3 max = new Vector3(xBounds.y, yBounds.y, 1f);
+            Vector3 size = max - min;
+            Gizmos.DrawWireCube(min + size / 2f, size);
         }
     }
 }
