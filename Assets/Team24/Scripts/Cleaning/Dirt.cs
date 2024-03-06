bce7b58f92abad9ad9f26eb5a460ed6549a9a76e
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-05 20:28:36 -0500

    Changed DIrt.CalculateCleanedPercent to be static

--- Assets/Team24/Scripts/Cleaning/Dirt.cs
+++ Assets/Team24/Scripts/Cleaning/Dirt.cs
@@ -6,22 +6,34 @@ namespace team24
 {
     public class Dirt : MonoBehaviour
     {
+        private static Dirt instance;
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        [Header("GPU dirt")]
         public RenderTexture cleanMask;
         [Range(0, 255)]
         public int clearThreshold = 15;
+
+        [Header("CPU dirt")]
+        public Vector2 dirtExtents;
+        public Vector2Int areaSubdivisions;
 
         /// <summary>
         /// Calculates what percent of the surface has been cleaned.
         /// </summary>
         /// <remarks>This function is expensive!</remarks>
         /// <returns></returns>
-        public float CalculateCleanedPercent()
+        public static float CalculateCleanedPercent()
         {
-            Texture2D buffer = new Texture2D(cleanMask.width, cleanMask.height, TextureFormat.R8, false);
-            RenderTexture.active = cleanMask;
+            RenderTexture rt = instance.cleanMask;
+            Texture2D buffer = new Texture2D(rt.width, rt.height, TextureFormat.R8, false);
+            RenderTexture.active = rt;
 
             // Read the mask into a texture
-            buffer.ReadPixels(new Rect(0, 0, cleanMask.width, cleanMask.height), 0, 0);
+            buffer.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
             buffer.filterMode = FilterMode.Bilinear;
 
             RenderTexture.active = null;
@@ -36,13 +48,36 @@ namespace team24
             int clearPixels = 0;
             for (int i = 0; i < colours.Length; i++)
             {
-                if (colours[i].r < clearThreshold)
+                if (colours[i].r < instance.clearThreshold)
                 {
                     clearPixels++;
                 }
             }
 
             return (float)clearPixels / colours.Length;
+        }
+
+
+        private void CreateCPUAreas()
+        {
+
+        }
+
+        /// <summary>
+        /// Defines an area of dirt on the CPU side
+        /// </summary>
+        class CPUArea
+        {
+            public Vector2 Min { get; private set; }
+            public Vector2 Center { get; private set; }
+            public Vector2 Size { get; private set; }
+
+            public CPUArea(Vector2 corner, Vector2 size)
+            {
+                Min = corner;
+                Size = size;
+                Center = Min + size / 2f;
+            }
         }
     }
 }
