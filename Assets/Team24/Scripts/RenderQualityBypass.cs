f825dea696699f9740106e4aa83d153d5813745d
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-05 23:24:08 -0500

    Tiny fixes

--- Assets/Team24/Scripts/RenderQualityBypass.cs
+++ Assets/Team24/Scripts/RenderQualityBypass.cs
@@ -11,23 +11,26 @@ public class RenderQualityBypass : MonoBehaviour
 {
     public UniversalRenderPipelineAsset rpAsset;
 
-    private void Awake()
-    {
-        BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
-        softShadowsField = typeof(UniversalRenderPipelineAsset).GetField(m_SoftShadowsSupported_FieldName, flags);
-    }
-
     int oldCascades; // 1 lol
     bool oldSoftShadows; // false :/
     FieldInfo softShadowsField;
     // Name of the field in UniversalRenderPipelineAsset
     static readonly string m_SoftShadowsSupported_FieldName = "m_SoftShadowsSupported";
 
+    private void Awake()
+    {
+        // We rippin out the reflection in here
+        BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
+        softShadowsField = typeof(UniversalRenderPipelineAsset).GetField(m_SoftShadowsSupported_FieldName, flags);
+    }
+
     private void OnEnable()
     {
+        // Save the old settings
         oldCascades = rpAsset.shadowCascadeCount;
         oldSoftShadows = rpAsset.supportsSoftShadows;
 
+        // Apply our devious HQ shadows
         rpAsset.shadowCascadeCount = 4;
         softShadowsField.SetValue(rpAsset, true);
     }
