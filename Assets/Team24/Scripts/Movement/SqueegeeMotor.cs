6be98c99369f6e7c7ed9e2540e738c63e8e97ce0
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-07 02:06:56 -0500

    Allowed swapping between wiper control modes (in editor)

--- Assets/Team24/Scripts/Movement/SqueegeeMotor.cs
+++ Assets/Team24/Scripts/Movement/SqueegeeMotor.cs
@@ -6,62 +6,73 @@ namespace team24
 {
     public class SqueegeeMotor : MicrogameInputEvents
     {
+        [SerializeField] ControlMode controlMode;
+        [SerializeField] GameObject squeegeeObj;
+        [SerializeField] float maxLength;
+
+        [Header("Point Towards Joystick")]
+        [SerializeField] float lerpSpeed = 5f;
+
+        [Header("In, Out, Rotate")]
+        [SerializeField] float moveLerpSpeed = 8f;
+        [SerializeField] float rotateSlerpSpeed = 4f;
+        [SerializeField] float rotationSpeed = 360f;
+        [SerializeField] float extensionSpeed = 5f;
+
         Vector2 direction;
         Vector2 desiredPos;
-        [SerializeField] float maxLength;
-        [SerializeField] GameObject squeegeeObj;
-        [SerializeField] float lerpSpeed;
-        [SerializeField] float rotationSpeed = 360f;
-        public float extensionSpeed = 5f;
 
-        //WiperPlayerMotor wiper;
-
-        //public bool deactivateMovement;
-
-        float targetAngle;
+        float targetAngle = 90;
         float targetLength;
 
-        #region Button overrides
-        /*
-        protected override void OnButton1Pressed(InputAction.CallbackContext context)
-        {
-
-            Debug.Log("Do action 1");
-
-        }
-
-        protected override void OnButton1Released(InputAction.CallbackContext context)
-        {
-
-            Debug.Log("Stop action 1");
-
-        }
-
-        protected override void OnButton2Pressed(InputAction.CallbackContext context)
-        {
-
-            Debug.Log("Do action 2");
-
-        }
-
-        protected override void OnButton2Released(InputAction.CallbackContext context)
-        {
-
-            Debug.Log("Stop action 2");
-
-        }
-        */
-        #endregion
-
-        private void Start()
-        {
-            //wiper = GetComponent<WiperPlayerMotor>();
-        }
 
         void Update()
         {
             direction = stick.normalized;
 
+            // Choose which controls we are using
+            if (controlMode == ControlMode.PointTowardsJoystick)
+                PointTowardsJoystick();
+            else
+                InOutRotate();
+        }
+
+        void PointTowardsJoystick()
+        {
+            bool movementIsDesired = stick != Vector2.zero;
+
+            if (movementIsDesired)
+                direction = stick.normalized;
+
+            //Handle rotation
+            Quaternion desiredRot = Quaternion.LookRotation(Vector3.forward, direction);
+
+            // If the squeegee is held out rotate it smoothly
+            if (Vector3.Distance(squeegeeObj.transform.position, transform.position) > 0.1f)
+                squeegeeObj.transform.rotation = Quaternion.Slerp(squeegeeObj.transform.rotation, desiredRot, lerpSpeed * Time.deltaTime);
+            // Otherwise it is close to our body - snap the rotation
+            else
+                squeegeeObj.transform.rotation = desiredRot;
+
+
+            Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+            desiredPos = currentPos + direction * maxLength;
+
+            //Handle movement
+            Vector2 pos2d;
+            // If we are trying to move the squeegee, move it towards the desired position
+            if (movementIsDesired)
+                pos2d = Vector2.Lerp(squeegeeObj.transform.position, desiredPos, lerpSpeed * Time.deltaTime);
+            // Otherwise bring it back home
+            else
+                pos2d = Vector2.Lerp(squeegeeObj.transform.position, transform.position, lerpSpeed * Time.deltaTime);
+
+
+            squeegeeObj.transform.position = new Vector3(pos2d.x, pos2d.y, squeegeeObj.transform.position.z);
+        }
+
+        void InOutRotate()
+        {
             // -= because rotation is backwards
             targetAngle -= direction.x * rotationSpeed * Time.deltaTime;
             // Make sure we don't go negative or too far
@@ -79,64 +90,14 @@ namespace team24
             Vector3 targetPos = transform.position + offset;
             targetPos.z = squeegeeObj.transform.position.z; // Keep it in line with the squeegee
 
-            squeegeeObj.transform.position = Vector3.Lerp(squeegeeObj.transform.position, targetPos, lerpSpeed * Time.deltaTime);
-            squeegeeObj.transform.rotation = Quaternion.Slerp(squeegeeObj.transform.rotation, targetRot, lerpSpeed * Time.deltaTime);
-           
+            squeegeeObj.transform.position = Vector3.Lerp(squeegeeObj.transform.position, targetPos, moveLerpSpeed * Time.deltaTime);
+            squeegeeObj.transform.rotation = Quaternion.Slerp(squeegeeObj.transform.rotation, targetRot, rotateSlerpSpeed * Time.deltaTime);
+        }
 
-            /*
-            if (stick != Vector2.zero)
-            {
-                direction = stick.normalized;
-            }
-
-            //Handle rotation
-            Quaternion desiredRot = Quaternion.LookRotation(Vector3.forward, direction);
-
-            if (Vector3.Distance(squeegeeObj.transform.position, transform.position) > 0.1f)
-            {
-                squeegeeObj.transform.rotation = Quaternion.Lerp(squeegeeObj.transform.rotation, desiredRot, lerpSpeed * Time.deltaTime);
-            }
-            else
-            {
-                squeegeeObj.transform.rotation = desiredRot;
-            }
-
-
-            Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
-
-            if (deactivateMovement)
-            {
-                wiper.enabled = false;
-                if (button1.IsPressed())
-                {
-                    desiredPos = currentPos + direction * maxLength;
-                }
-                else
-                {
-                    desiredPos = currentPos + direction;
-                }
-            }
-            else
-            {
-                wiper.enabled = true;
-                desiredPos = currentPos + direction * maxLength;
-            }
-
-
-            //Handle movement
-            Vector2 pos2d;
-            if (stick == Vector2.zero) 
-            {
-                pos2d = Vector2.Lerp(squeegeeObj.transform.position, transform.position, lerpSpeed * Time.deltaTime);
-            }
-            else
-            {
-                pos2d = Vector2.Lerp(squeegeeObj.transform.position, desiredPos, lerpSpeed * Time.deltaTime);
-            }
-
-
-            squeegeeObj.transform.position = new Vector3(pos2d.x, pos2d.y, squeegeeObj.transform.position.z);
-            */
+        public enum ControlMode
+        {
+            PointTowardsJoystick,
+            InOutRotate,
         }
     }
 }
