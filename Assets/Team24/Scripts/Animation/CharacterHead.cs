0ea630f6d9fa555cecf48e0d237b6fd6c6dcff0d
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-07 17:46:06 -0500

    Characters look towards squeegee

--- Assets/Team24/Scripts/Animation/CharacterHead.cs
+++ Assets/Team24/Scripts/Animation/CharacterHead.cs
@@ -14,12 +14,33 @@ namespace team24
         public float maxAngle = 30f;
         public Vector3 offset;
 
+        Vector3 baseDirection;
+        Quaternion baseRotation;
+
+        private void Start()
+        {
+            baseDirection = transform.forward;
+            baseRotation = transform.rotation;
+        }
+
         private void LateUpdate()
         {
             // Find what way we should face
-            Vector3 direction = lookTarget.position - transform.position;
+            Vector3 direction;
+            if (lookTarget != null)
+                direction = lookTarget.position - transform.position;
+            else
+                direction = baseDirection;
             Vector3 rotatedDirection = Quaternion.Euler(offset) * direction;
-            Quaternion rot = Quaternion.LookRotation(rotatedDirection);
+            
+            // Clamp the angle of said rotation
+            Quaternion delta = Quaternion.FromToRotation(baseDirection, rotatedDirection);
+            delta.ToAngleAxis(out float angle, out Vector3 axis);
+            // Add the delta to our base rotation
+            Quaternion rotation = baseRotation * Quaternion.AngleAxis(Mathf.Clamp(angle, -maxAngle, maxAngle), axis);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * slerpSpeed);
+
             //Quaternion.ang
         }
     }
