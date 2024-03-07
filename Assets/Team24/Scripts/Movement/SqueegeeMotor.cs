648e512efa904ff92ff4a6f085fa862622e45d9c
Jonathan To <jonto398@gmail.com>
2024-03-06 23:44:10 -0500

    wip squeegee changes, may toss out

--- Assets/Team24/Scripts/Movement/SqueegeeMotor.cs
+++ Assets/Team24/Scripts/Movement/SqueegeeMotor.cs
@@ -17,6 +17,8 @@ namespace team24
         WiperPlayerMotor wiper;
 
         public bool deactivateMovement;
+        float targetAngle;
+        float targetLength;
 
         #region Button overrides
         /*
@@ -59,72 +61,72 @@ namespace team24
         {
             direction = stick.normalized;
 
-            /*//Handle rotation
-            /*Quaternion desiredRot = Quaternion.LookRotation(Vector3.forward, direction);
+                /*//Handle rotation
+                /*Quaternion desiredRot = Quaternion.LookRotation(Vector3.forward, direction);
 
-            if (Vector3.Distance(squeegeeObj.transform.position, transform.position) > 0.1f)
-            {
-                squeegeeObj.transform.rotation = Quaternion.Lerp(squeegeeObj.transform.rotation, desiredRot, lerpSpeed * Time.deltaTime);
-            }
-            else
-            {
-                squeegeeObj.transform.rotation = desiredRot;
-            }
-
-
-            Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
-
-            if (deactivateMovement)
-            {
-                wiper.enabled = false;
-                if (button1.IsPressed())
+                if (Vector3.Distance(squeegeeObj.transform.position, transform.position) > 0.1f)
                 {
-                    desiredPos = currentPos + direction * maxLength;
+                    squeegeeObj.transform.rotation = Quaternion.Lerp(squeegeeObj.transform.rotation, desiredRot, lerpSpeed * Time.deltaTime);
                 }
                 else
                 {
-                    desiredPos = currentPos + direction;
+                    squeegeeObj.transform.rotation = desiredRot;
                 }
-            }
-            else
-            {
-                wiper.enabled = true;
-                desiredPos = currentPos + direction * maxLength;
-            }
 
 
-            //Handle movement
-            Vector2 pos2d;
-            
-            if (stick == Vector2.zero) 
-            {
-                pos2d = Vector2.Lerp(squeegeeObj.transform.position, transform.position, lerpSpeed * Time.deltaTime);
-            }
-            else
-            {
-                pos2d = Vector2.Lerp(squeegeeObj.transform.position, transform.position, lerpSpeed * Time.deltaTime);
-            }
+                Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
 
-            squeegeeObj.transform.position = new Vector3(pos2d.x, pos2d.y, squeegeeObj.transform.position.z);
-            */
+                if (deactivateMovement)
+                {
+                    wiper.enabled = false;
+                    if (button1.IsPressed())
+                    {
+                        desiredPos = currentPos + direction * maxLength;
+                    }
+                    else
+                    {
+                        desiredPos = currentPos + direction;
+                    }
+                }
+                else
+                {
+                    wiper.enabled = true;
+                    desiredPos = currentPos + direction * maxLength;
+                }
 
-            Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
-            Vector2 pos2d = new Vector2(squeegeeObj.transform.position.x, squeegeeObj.transform.position.y);
 
-            desiredPos = currentPos + new Vector2(squeegeeObj.transform.up.x, squeegeeObj.transform.up.y) * maxLength;
+                //Handle movement
+                Vector2 pos2d;
+
+                if (stick == Vector2.zero) 
+                {
+                    pos2d = Vector2.Lerp(squeegeeObj.transform.position, transform.position, lerpSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    pos2d = Vector2.Lerp(squeegeeObj.transform.position, transform.position, lerpSpeed * Time.deltaTime);
+                }
+
+                squeegeeObj.transform.position = new Vector3(pos2d.x, pos2d.y, squeegeeObj.transform.position.z);
+                */
+
+            //Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+            //Vector2 pos2d = new Vector2(squeegeeObj.transform.position.x, squeegeeObj.transform.position.y);
+
+            //desiredPos = currentPos + new Vector2(squeegeeObj.transform.up.x, squeegeeObj.transform.up.y) * maxLength;
 
             if (direction != Vector2.zero)
             {
                 if (direction.y > 0)
                 {
-                    if (squeegeeObj.transform.position.y < maxLength)
+                    if (Vector3.Distance(squeegeeObj.transform.position, transform.position) < maxLength)
                     {
-                        pos2d = Vector2.Lerp(squeegeeObj.transform.position, desiredPos, extensionSpeed * Time.deltaTime);
+                        squeegeeObj.transform.Translate(direction * extensionSpeed * Time.deltaTime, Space.Self);
                     }
                 }
                 else if (direction.y < 0)
                 {
-                    pos2d = Vector2.Lerp(squeegeeObj.transform.position, transform.position, extensionSpeed * Time.deltaTime);
+                    squeegeeObj.transform.position = Vector2.Lerp(squeegeeObj.transform.position, new Vector2(transform.position.x, transform.position.y), extensionSpeed * Time.deltaTime);
                 }
                 if (direction.x < 0)
                 {
@@ -136,7 +138,7 @@ namespace team24
                 }
             }
 
-            squeegeeObj.transform.position = new Vector3(pos2d.x, pos2d.y, squeegeeObj.transform.position.z);
+            //squeegeeObj.transform.position = new Vector3(pos2d.x, pos2d.y, squeegeeObj.transform.position.z);
         }
     }
 }
