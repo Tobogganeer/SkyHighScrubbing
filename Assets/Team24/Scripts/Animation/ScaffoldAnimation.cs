e31e9af0f954c39150d0451e94e7cade6f189961
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-03-07 21:32:52 -0500

    Finished curves and tests

--- Assets/Team24/Scripts/Animation/ScaffoldAnimation.cs
+++ Assets/Team24/Scripts/Animation/ScaffoldAnimation.cs
@@ -10,7 +10,8 @@ namespace team24
         public AnimationCurve arrivalCurve;
         public AnimationCurve departureCurve;
         public Transform topWaitingPoint;
-        public float travelTime = 1.5f;
+        public float arrivalTime = 2.0f;
+        public float departureTime = 2.5f;
 
         PhysicsScaffoldMotor scaffold;
         Rigidbody rb;
@@ -35,11 +36,11 @@ namespace team24
 
             float time = 0;
 
-            while (time < travelTime)
+            while (time < arrivalTime)
             {
                 // Move towards our target
                 time += Time.deltaTime;
-                float fac = time / travelTime;
+                float fac = Mathf.Clamp01(time / arrivalTime);
                 float interpFactor = arrivalCurve.Evaluate(fac);
                 transform.position = Vector3.LerpUnclamped(start, target, interpFactor);
                 yield return null;
@@ -61,15 +62,15 @@ namespace team24
 
             // We want to get here
             target = topWaitingPoint.position;
-            start = topWaitingPoint.position;
+            start = transform.position;
 
             float time = 0;
 
-            while (time < travelTime)
+            while (time < departureTime)
             {
                 // Move towards our target
                 time += Time.deltaTime;
-                float fac = time / travelTime;
+                float fac = Mathf.Clamp01(time / departureTime);
                 float interpFactor = departureCurve.Evaluate(fac);
                 transform.position = Vector3.LerpUnclamped(start, target, interpFactor);
                 yield return null;
